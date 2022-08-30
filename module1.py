4d5766036c8571cdd9d14d6db61795aef1fd667d
sofiwayne <101227764+sofiwayne@users.noreply.github.com>
2022-08-30 20:46:57 -0300

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      inap
+#
+# Created:     30/08/2022
+# Copyright:   (c) inap 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+n1 = (int (input("ingrese numero impar: ")))
+while n1 % 2 == 0:
+    n1 = (int (input("ingrese numero impar: ")))
+print ("número impar correcto")
